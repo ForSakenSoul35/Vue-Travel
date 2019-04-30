bea155d010e40d16d7186064a2dd4c83a6172cf6
yaya <forsakensoul35@qq.com>
2019-05-01 02:53:29 +0800

    vue project

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <div>home</div>
+    <home-header></home-header>
     <!-- router-link用来跳转页面  to属性表示跳转到哪里 -->
-    <!-- 单页应用：页面跳转通过js重新渲染完成，优点是页面切换快，缺点是，首屏时间加载稍慢，SEO差-->
+    <!-- 单页应用：页面跳转通过js重新渲染完成，优点是页面切换快，缺点是，首屏时间加载稍慢 SEO较差-->
     <!-- <router-link to="/list" >列表页</router-link> -->
   </div>
 </template>
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="header-left"><div class="iconfont back-icon">&#xe623;</div></div>
+    <div class="header-input"><span class="iconfont input">&#xe6a8;</span>输入城市/景点/游玩/主题</div>
+    <div class="header-right">城市<span class="iconfont arrow-icon">&#xe600;</span></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+@import '../../../assets/styles/varibles.styl'
+.header
+  display: flex
+  background-color :$bgColor
+  color: #ffffff
+  line-height: 0.86rem
+  height: .86rem
+  .header-left
+    width: .64rem
+    float: left
+    .back-icon
+      text-align: center
+      font-size: .5rem
+  .header-input
+    flex: 1
+    height: .64rem
+    line-height: .64rem
+    margin-top: .12rem
+    margin-left: .15rem
+    padding-left: .2rem
+    background-color #fff
+    border-radius: .1rem
+    color: #ccc
+    font-size: .3rem
+    overflow hidden
+    .input
+      margin-right: .2rem
+  .header-right
+    width: 1.2rem
+    float: right
+    text-align: center
+    font-size: .26rem
+    .arrow-icon
+      font-size: .24rem
+      margin-left: .1rem    
+</style>
