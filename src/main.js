eb38f5b726e913ece2a92d5208229eedeaa4c9b9
yaya <forsakensoul35@qq.com>
2019-04-30 22:45:25 +0800

    vue project

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 入口文件
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+// 执行index.html
+// html 上有个 id="app"的 div 表示新建的vue实例 挂载到这个地方
+// 新建的vue实例中有组件 App以及模板
+//
 new Vue({
   el: '#app',
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+// @ 指的是src文件夹
+import Home from '@/pages/home/Home'
+import List from '@/pages/list/List'
 Vue.use(Router)
 
 export default new Router({
+  // 定义path
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      // 根路由对应的是 helloworld组件
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
+// 路由就是根据网址的不同 返回给用户的内容不同
